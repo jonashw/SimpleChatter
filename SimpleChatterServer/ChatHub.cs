d5b7c2898494f9caf06e1e71900d18e9bad92ca0
Jon Wilson <2jonwilson@gmail.com>
2018-08-15 10:40:10 -0700

    added temporary persistence for messages (static member) so that newly-connected chatters see the full history of messages; added 'so-and-so is typing' feature

--- SimpleChatterServer/ChatHub.cs
+++ SimpleChatterServer/ChatHub.cs
@@ -1,12 +1,55 @@
 ﻿using Microsoft.AspNet.SignalR;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace SimpleChatterServer
 {
+    public class Message
+    {
+        public readonly string UserName;
+        public readonly string Text;
+
+        public Message(string userName, string text)
+        {
+            UserName = userName;
+            Text = text;
+        }
+    }
+
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        private static List<string> TypingNames = new List<string>();
+        private static List<Message> Messages = new List<Message>();
+
+        public void PostMessage(string name, string message)
         {
-            Clients.All.broadcastMessage(name, message);
+            Messages.Add(new Message(name, message));
+            Clients.All.MessagePosted(name, message);
+        }
+
+        public void StartedTyping(string name)
+        {
+            TypingNames.Add(name);
+            Clients.All.StartedTyping(name);
+        }
+
+        public void StoppedTyping(string name)
+        {
+            TypingNames.Remove(name);
+            Clients.All.StoppedTyping(name);
+        }
+
+        public override Task OnConnected()
+        {
+            foreach(var m in Messages)
+            {
+                Clients.Caller.MessagePosted(m.UserName, m.Text);
+            }
+            foreach(var name in TypingNames)
+            {
+                Clients.Caller.StartedTyping(name);
+            }
+            return base.OnConnected();
         }
     }
 }
---
+++ SimpleChatterServer/Scripts/ChatMessagesComponent.js
@@ -0,0 +1,18 @@
+﻿function ChatMessagesComponent(userName, $log, $form, $input, chat) {
+	chat.client.messagePosted = function (name, message) {
+		var nameMessagePair = $('<div></div>')
+			.append($('<span></span>').addClass('badge badge-default').text(name))
+			.append($('<span></span>').text(' ' + message));
+		$log.append(nameMessagePair);
+		$log.prop('scrollTop', $log.prop('scrollHeight'));
+	};
+
+	this.start = () =>
+	{
+		$form.submit(function (e) {
+			chat.server.postMessage(userName, $input.val());
+			$input.val('').focus();
+			return false;
+		});
+	};
+}
